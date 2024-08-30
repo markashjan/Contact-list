e12535cdfefa0f7e5d7e513c91e526616f947c13
markashjan <166384963+markashjan@users.noreply.github.com>
2024-08-30 15:43:11 +0800

    preparing for authentication routings

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ContactController;
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [ContactController::class, 'index']);
@@ -11,3 +12,5 @@ Route::post('update/{contact}', [ContactController::class, 'update']);
 Route::post('store_data', [ContactController::class, 'store']);
 Route::get('details/delete/{contact}', [ContactController::class, 'delete']);
 Route::get('search',[ContactController::class, 'search']);
+//To Do Authentication
+#Auth::routes();
