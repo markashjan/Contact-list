fba61f0559ed54ef5aeb3c6b778e76662148a2f5
markashjan <166384963+markashjan@users.noreply.github.com>
2024-08-30 13:13:13 +0800

    done basic search ui

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,7 +5,13 @@
 @section('content')
 
     <div class="row mt-3">
-        <div class="col-12 align-self-center">
+        <div class="col-12 p-3  align-self-center">
+            <div class="p-3">
+                <form action="" method="GET">
+                    <input type="text" name="search" placeholder="Search Contact">
+                    <button type="submit">Search</button>
+                </form>
+            </div>        
             <ul class="list-group">
                 @foreach($Contacts as $view)
                 <li class="list-group-item"><a href="details/{{$view->id}}">{{$view->name}}</a></li>
